f581931138395e51a411e3315189a9d8cd22e097
chilimilk <294257363@qq.com>
2020-12-22 17:23:47 +0800

    Update Name

--- Assets/ChiliMilkToonShader/ToonForwardRender/ToonForwardRenderer.cs
+++ Assets/ChiliMilkToonShader/ToonForwardRender/ToonForwardRenderer.cs
@@ -15,7 +15,7 @@ namespace UnityEngine.Rendering.Universal
 
         private static class Profiling
         {
-            private const string k_Name = nameof(ForwardRenderer);
+            private const string k_Name = nameof(ToonForwardRenderer);
             public static readonly ProfilingSampler createCameraRenderTarget = new ProfilingSampler($"{k_Name}.{nameof(CreateCameraRenderTarget)}");
         }
 
